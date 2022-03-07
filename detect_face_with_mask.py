f0ceffffa2edd2a1bc6ea37835d3532e91eb268c
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-03-07 14:15:11 -0600

    Traducción 1.0

--- detect_face_with_mask.py
+++ detect_face_with_mask.py
@@ -1,25 +1,25 @@
-# import the opencv library
+# Importar la biblioteca OpenCV
 import cv2
   
-# define a video capture object
+# Definir un objeto de captura de video
 vid = cv2.VideoCapture(0)
   
 while(True):
       
-    # Capture the video frame by frame
+    # Capturar el video fotograma por fotograma
     ret, frame = vid.read()
   
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+    # Mostrar el fotograma resultante
+    cv2.imshow('Fotograma', frame)
       
-    # Quit window with spacebar
+    # Salir de la ventana con la barra espaciadora
     key = cv2.waitKey(1)
     
     if key == 32:
         break
   
-# After the loop release the cap object
+# Después del bucle, liberar al objeto de captura
 vid.release()
 
-# Destroy all the windows
+# Destruir todas las ventanas
 cv2.destroyAllWindows()
